bec98ce9a937697a072d43971e89623e70e901dc
Altay Murat <amurat@echelon-software.com>
2022-09-27 15:58:44 -0400

    WIP gl2 renderer

--- src/rendergles2.cpp
+++ src/rendergles2.cpp
@@ -1,9 +1,5 @@
-#include <SDL.h>
-
 #include <iostream>
-
-extern void RunGLES2Renderer(SDL_Window *);
-
+#include <SDL.h>
 #include "angle_gl.h"
 
 namespace {
@@ -84,56 +80,58 @@ GLuint loadProgram(const GLchar* f_vertSource_p, const GLchar* f_fragSource_p) {
 }
 }  // namespace
 
-int main(int, char**) {
-  // Init SDL
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
-    std::cerr << SDL_GetError() << std::endl;
-    return EXIT_FAILURE;
-  }
 
-  SDL_version version;
-  SDL_GetVersion(&version);
-  std::cout << "SDL version: " << static_cast<int>(version.major) << "."
-            << static_cast<int>(version.minor) << "."
-            << static_cast<int>(version.patch) << std::endl;
+void RunGLES2Renderer(SDL_Window *window)
+{
+    std::cout << "GL version: " << glGetString(GL_VERSION) << std::endl;
 
-  // Create window
-  const bool bUseGLES = true;
-    
-  if (bUseGLES) {
-      SDL_SetHint(SDL_HINT_OPENGL_ES_DRIVER, "1");
-      SDL_GL_SetAttribute(SDL_GL_CONTEXT_EGL, 1);
-      SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-  } else {
-      SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-      SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-      SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-  }
-    
-  // Explicitly set channel depths, otherwise we might get some < 8
-  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    // Load shader program
+    constexpr char kVS[] = R"(attribute vec4 vPosition;
+  void main()
+  {
+      gl_Position = vPosition;
+  })";
 
-  auto windowFlags = SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL;
-  auto window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED,
-                                 SDL_WINDOWPOS_CENTERED, 512, 512, windowFlags);
+    constexpr char kFS[] = R"(precision mediump float;
+  void main()
+  {
+      gl_FragColor = vec4(gl_FragCoord.x / 512.0, gl_FragCoord.y / 512.0, 0.0, 0.1);
+  })";
 
-  // Init GL
-  auto glContext = SDL_GL_CreateContext(window);
-  SDL_GL_MakeCurrent(window, glContext);
-  if (bUseGLES) {
-    RunGLES2Renderer(window);
-  } else {
-        
-  }
+    auto program = loadProgram(kVS, kFS);
 
-    // Clean up
-  SDL_GL_DeleteContext(glContext);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
-  return EXIT_SUCCESS;
+    // Main loop
+    bool isRunning = true;
+    while (isRunning) {
+      SDL_Event event;
+      while (0 != SDL_PollEvent(&event)) {
+        if (SDL_QUIT == event.type) {
+          isRunning = false;
+        } else if (SDL_KEYDOWN == event.type) {
+          const auto keyStates_p = SDL_GetKeyboardState(nullptr);
+          if (keyStates_p[SDL_SCANCODE_ESCAPE]) {
+            isRunning = false;
+          }
+        }
+      }
+
+      // Clear
+      glClearColor(0.2F, 0.2F, 0.2F, 1.F);
+      glClear(GL_COLOR_BUFFER_BIT);
+      glViewport(0, 0, 512, 512);
+
+      // Render scene
+      GLfloat vertices[] = {
+          0.0f, 0.5f, 0.0f, -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f,
+      };
+      glUseProgram(program);
+      glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertices);
+      glEnableVertexAttribArray(0);
+      glDrawArrays(GL_TRIANGLES, 0, 3);
+
+      // Update window
+      SDL_GL_SwapWindow(window);
+    }
+
 }
+
